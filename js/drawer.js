d7b53e557b71682cf8e282398d5ed0c04de668ed
mohamed-Github <mohamed.hussein151515@gmail.com>
2022-04-05 13:23:46 +0200

    some updates and added jquery

--- js/drawer.js
+++ js/drawer.js
@@ -1,24 +1,42 @@
-const drawer = document.querySelector(".drawer");
-const drawerBg = document.querySelector(".drawer-background");
+// window.addEventListener('load', () => {
+//   const drawer = document.querySelector(".drawer");
+//   const drawerBg = document.querySelector(".drawer-background");
+//   const drawerCheckbox = document.querySelector("input.drawer-checkbox");
 
-const drawerCheckbox = document.querySelector("input.drawer-checkbox");
+//   drawerCheckbox.checked = false;
 
-window.addEventListener('load', () => {
-  drawerCheckbox.checked = false;
-});
+//   drawerCheckbox.addEventListener("change", function () {
+//     if (this.checked) {
+//       drawer.classList.add("visible");
+//       drawerBg.classList.add("visible");
+//     } else {
+//       drawer.classList.remove("visible");
+//       drawerBg.classList.remove("visible");
+//     }
+//   });
+  
+//   drawerBg.addEventListener("click", () => {
+//     drawerCheckbox.checked = false;
+//     drawer.classList.remove("visible");
+//     drawerBg.classList.remove("visible");
+//   });
+// });
 
-drawerCheckbox.addEventListener("change", function () {
-  if (this.checked) {
-    drawer.classList.add("visible");
-    drawerBg.classList.add("visible");
-  } else {
-    drawer.classList.remove("visible");
-    drawerBg.classList.remove("visible");
-  }
-});
+$(document).ready(function() {
+  const drawer = $(".drawer");
+  const drawerBg = $(".drawer-background");
+  const drawerCheckbox = $("input.drawer-checkbox");
 
-drawerBg.addEventListener("click", () => {
-  drawerCheckbox.checked = false;
-  drawer.classList.remove("visible");
-  drawerBg.classList.remove("visible");
+  drawerCheckbox.prop("checked", false);
+
+  drawerCheckbox.change(function () {
+    drawer.toggleClass("visible");
+    drawerBg.toggleClass("visible");
+  });
+
+  drawerBg.click(function () {
+    drawerCheckbox.prop("checked", false);
+    drawer.toggleClass("visible");
+    drawerBg.toggleClass("visible");
+  });
 });
