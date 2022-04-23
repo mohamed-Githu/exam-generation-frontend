cba6a227e07e52eca091c1c5655aa736b56a98dc
mohamed-Github <mohamed.hussein151515@gmail.com>
2022-04-23 16:33:33 +0200

    updates & finished subarbuments components

---
+++ js/blocks-form.js
@@ -0,0 +1,56 @@
+const handleBlocks = (value) => {
+  const blocksContainer = $(".blocks-form .flex-wrapper");
+  const blocksCount = $(".blocks-count");
+  blocksContainer.empty();
+
+  if (!value.trim()) {
+    blocksCount.text("0");
+    return;
+  }
+
+  value.split("\n").forEach((text, i) => {
+    const newBlock = $(
+      `<div class='text-block dark'>
+        ${text}
+        <i class='material-icons'>close</i>
+      </div>`
+    );
+
+    newBlock.click(() => removeBlock(i));
+
+    blocksContainer.append(newBlock);
+  });
+
+  blocksCount.text(value.split("\n").length);
+};
+
+const removeBlock = (index) => {
+  const textarea = $(".blocks-area");
+  const blocks = textarea.val().split("\n");
+
+  blocks.splice(index, 1);
+  textarea.val(blocks.join("\n"));
+  handleBlocks(textarea.val());
+}
+
+$(document).ready(function () {
+  handleBlocks($(".blocks-area").val());
+
+  $(".blocks-form input.block-input").keypress(function (e) {
+    if (e.which != 13) return;
+    e.preventDefault();
+
+    if (!$(this).val().trim()) return;
+
+    const newBlock = $(this).val().trim();
+    const textarea = $(".blocks-area");
+    const blocksValue = textarea.val();
+
+    blocksValue
+      ? textarea.val(blocksValue.trim() + "\n" + newBlock)
+      : textarea.val(blocksValue.trim() + newBlock);
+
+    handleBlocks(textarea.val());
+    $(this).val("");
+  });
+});
