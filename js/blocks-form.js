e51774740f24b0c10a43da6890915aaf5006fa9a
mohamed-Github <mohamed.hussein151515@gmail.com>
2022-05-01 15:08:17 +0200

    added the functionality of modifiying the subjects of each teacher in teacher.html

--- js/blocks-form.js
+++ js/blocks-form.js
@@ -31,7 +31,7 @@ const removeBlock = (index) => {
   blocks.splice(index, 1);
   textarea.val(blocks.join("\n"));
   handleBlocks(textarea.val());
-}
+};
 
 $(document).ready(function () {
   handleBlocks($(".blocks-area").val());
@@ -52,5 +52,55 @@ $(document).ready(function () {
 
     handleBlocks(textarea.val());
     $(this).val("");
+  });
+
+  $(".blocks-form input.block-select").keyup(function (e) {
+    const inputList = $(this).parent(".input-group").find(".input-list");
+    const inputValue = $(this).val().trim().toLowerCase();
+    const textarea = $(this).parents(".blocks-form").find(".blocks-area");
+    const blocksValue = textarea.val().toLowerCase();
+
+    if (!inputValue) {
+      inputList.hide();
+      return;
+    }
+
+    inputList.show();
+
+    let count = 5;
+
+    inputList.find("li").each(function (i) {
+      const text = $(this).text().toLowerCase();
+
+      if (
+        (count > 0) &
+        text.includes(inputValue) &
+        !blocksValue.includes(text)
+      ) {
+        $(this).show();
+        count--;
+      } else {
+        $(this).hide();
+      }
+    });
+  });
+
+
+  $(".blocks-form ul.input-list li").click(function() {
+    const textarea = $(this).parents(".blocks-form").find(".blocks-area");
+    const inputSelect = $(this).parents(".blocks-form").find(".block-select");
+    const inputList = $(this).parent();
+    const newBlock = $(this).text();
+
+    const blocksValue = textarea.val();
+
+    blocksValue
+      ? textarea.val(blocksValue.trim() + "\n" + newBlock)
+      : textarea.val(blocksValue.trim() + newBlock);
+
+    handleBlocks(textarea.val());
+    inputList.hide();
+    inputSelect.val("");
+    inputSelect.focus();
   });
 });
