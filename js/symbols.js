65327d259164016d88b5dd67dd3dca621d11ee77
mohamed-Github <mohamed.hussein151515@gmail.com>
2022-06-01 11:48:44 +0200

    added the symbols.css and the symbols section in the nuova-domanda.html + it's functionality

--- js/symbols.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $("#symbol-input").keyup(function() {
-    const previewText = "\\( " + $(this).val() + " \\)";
-
-    $("#symbol-preview").text(previewText);
-    MathJax.typeset();
-  });
-});
