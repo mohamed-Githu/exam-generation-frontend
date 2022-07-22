e5ca28d06e472335eca6c0af4f43dfd9bde5a941
mohamed-Github <mohamed.hussein151515@gmail.com>
2022-07-22 17:23:32 +0200

    adjusted the dropdown-menu component for the unita' and finished the modals with the respective functionality

--- js/slide-menu.js
+++ js/slide-menu.js
@@ -4,7 +4,7 @@ window.addEventListener("load", () => {
   toggles.forEach(function (toggle) {
     toggle.addEventListener("click", function () {
       const panel = this.nextElementSibling;
-      const icon = $(this).find("i")[0];
+      const icon = $(this).find("i.arrow")[0];
       if (panel.style.maxHeight) {
         panel.style.maxHeight = null;
         icon.style.transform = "rotate(0deg)";
@@ -13,5 +13,29 @@ window.addEventListener("load", () => {
         icon.style.transform = "rotate(90deg)";
       }
     });
+  });
+
+  $(".toggle i.delete").click(function(e) {
+    e.stopPropagation();
+    const id = $(this).parents(".dropdown-menu").prop("id");
+    const titolo = $(this).parents(".dropdown-menu").children().find(".unita-titolo").text();
+    const modal = $("#rimvuovi-unita");
+
+    modal.children().find(".unita-titolo").text(titolo);
+    modal.children().find("input[name='unita-id']").val(id);
+    
+    location.hash = "#rimvuovi-unita";
+  });
+
+  $(".toggle i.add").click(function(e) {
+    e.stopPropagation();
+    const id = $(this).parents(".dropdown-menu").prop("id");
+    const titolo = $(this).parents(".dropdown-menu").children().find(".unita-titolo").text();
+    const modal = $("#argument-modal");
+
+    modal.children().find(".unita-titolo").text(titolo);
+    modal.children().find("input[name='unita-id']").val(id);
+    
+    location.hash = "#argument-modal";
   });
 });
